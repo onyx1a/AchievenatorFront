06e4e1d876cf2d73e27dab832bc1c3f02933656f
onyx1a <onyx1anis@gmail.com>
2023-04-25 08:16:33 +0300

    First view on game list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SteamIDForm from './SteamIDForm';
 
 function App() {
+  const [gameData, setGameData] = useState({});
+  const [showFullDone, setShowFullDone] = useState(false);
+
+  console.log(gameData);
+  let has_data = Object.keys(gameData).length !== 0;
+  console.log(has_data);
+
+  if (has_data) {
+    if (!showFullDone) {
+      gameData.game_data = gameData.game_data.filter(item => item.achievements_count - item.achievements_done != 0);
+    }
+
+    gameData.game_data.sort((a, b) => {
+      var scoreA = a.achievements_count - a.achievements_done;
+      var scoreB = b.achievements_count - b.achievements_done;
+      if (scoreA < scoreB) {
+        return -1;
+      }
+      if (scoreA > scoreB) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      <body className="App-body">
+        <SteamIDForm onDataReceived={setGameData}/>
+        {has_data && (
+          <div>
+            <div>
+              Games: {gameData.game_data.length}<br/>
+              Achievements: {gameData.overall_done_ach_count}/{gameData.overall_ach_count}<br/>
+              Total score: {gameData.overall_ach_count-gameData.overall_done_ach_count} (?)<br/>
+            </div>
+            <ul>
+              {gameData.game_data.map(game => (
+                <li key={game.app_id}>
+                  <div className='game-item'>
+                    <img className='game-logo' key={game.app_id} src={`https://cdn.cloudflare.steamstatic.com/steam/apps/${game.app_id}/header.jpg`} alt={game.title}/>
+                    <div className='game-info'>
+                      <div className='game-title'><a href={`https://store.steampowered.com/app/${game.app_id}`} target="_blank">{game.title}</a></div>
+                      <div className='game-desc'>Achievements: {game.achievements_done}/{game.achievements_count}. Score: {game.achievements_count-game.achievements_done}</div>
+                      <div className='game-icons'>
+                        <img className="game-icon" src={`http://media.steampowered.com/steamcommunity/public/images/apps/${game.app_id}/${game.img_icon_url_hash}.jpg`}/>
+                        <img className="game-icon" src={`http://media.steampowered.com/steamcommunity/public/images/apps/${game.app_id}/${game.img_icon_url_hash}.jpg`}/>
+                        <img className="game-icon" src={`http://media.steampowered.com/steamcommunity/public/images/apps/${game.app_id}/${game.img_icon_url_hash}.jpg`}/>
+                        <img className="game-icon" src={`http://media.steampowered.com/steamcommunity/public/images/apps/${game.app_id}/${game.img_icon_url_hash}.jpg`}/>
+                        <img className="game-icon" src={`http://media.steampowered.com/steamcommunity/public/images/apps/${game.app_id}/${game.img_icon_url_hash}.jpg`}/>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul> 
+          </div>
+        )}
+      </body>
     </div>
   );
 }
---
+++ src/SteamIDForm.js
@@ -0,0 +1,77 @@
+import React, {useState} from 'react';
+import Button from 'react-bootstrap/Button'
+import Spinner from 'react-bootstrap/Spinner';
+import Form from 'react-bootstrap/Form'
+
+function SteamIDForm(props) {
+  const {onDataReceived} = props;
+  const [query, setQuery] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleQueryChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    setIsLoading(true);
+    event.preventDefault();
+    const regex = /(?:\/(id|profiles)\/([\w-]+))|(?:^(\d{17,}))$/;
+    const result = query.match(regex);
+    let steamid = "-1"
+    if (result) {
+      // console.log(result[1], result[2]); 
+      console.log(result);
+      if (result[1] === "id") {
+        // Search steamid by profile name (result[2])
+      } else if (result[1] === "profiles") {
+        steamid = result[2];
+      } else if (result[1] === undefined) {
+        steamid = result[3];
+      } else {
+        // User invalid
+      }
+    } else {
+      console.log("Invalid");
+      setIsLoading(false);
+    }
+    if (steamid !== "-1") {
+        fetch(`/data/${query}`)
+          .then((response) => response.json())
+          .then((data) => {
+            setIsLoading(false);
+            onDataReceived(data);
+          })
+          .catch((error) => {
+            setIsLoading(false);
+            console.error(error);
+          });
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      	<Form.Group className="mb-3">
+            <Form.Label>Profile URL / Profile Name / SteamID:</Form.Label>
+            <Form.Control id='query' onChange={handleQueryChange}/>
+            <Form.Text muted>
+              https://steamcommunity.com/id/onyx1a / onyx1a / 76561198089758289
+            </Form.Text>
+        </Form.Group>
+        <Button variant="primary" type="submit" disabled={isLoading}>
+          {isLoading ? (
+            <div>
+              <Spinner
+                as="span"
+                size="sm"
+                role="status"
+                aria-hidden="true"
+              />
+              Loading...
+            </div>
+          ): "Search"}
+        </Button>
+    </Form>
+  );
+}
+
+export default SteamIDForm;
