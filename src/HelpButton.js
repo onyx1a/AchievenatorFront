9ae58a236a4abeea1a0ad8a82cabcc0ed5d2d8bf
onyx1a <onyx1anis@gmail.com>
2023-05-01 14:44:36 +0300

    Added component with tooltip

---
+++ src/HelpButton.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BsQuestionCircle } from 'react-icons/bs'
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
+
+
+const HelpButton = (props) => {
+    const { text } = props;
+
+    const renderTooltip = (props) => (
+        <Tooltip {...props}>
+            { text }
+        </Tooltip>
+    );
+
+    return(
+        <OverlayTrigger placement="right" overlay={renderTooltip}>
+            <span>
+                <BsQuestionCircle className="question-icon"/>
+            </span>
+        </OverlayTrigger>
+    );
+};
+
+export default HelpButton;
